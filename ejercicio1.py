a082832332555413594320eee56d69dbe941bea7
davidsequeiro <david.sequeiro@gmail.com>
2024-03-17 12:00:44 +0100

    Prework ejercicio1 python

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,12 @@
-#prueba
+'''
+Ejercicio 1: Conversor de Temperatura 
+Escribe un programa que convierta una temperatura de grados Celsius a grados Fahrenheit.
+'''
+
+a = 32  # Temperatura en grados Celsius
+print(a)
+b = a*(9/5)+32  # Formula de conversion Celsius a Farenheit
+print(b)
+c = a,'Celsius, es igual a ',b,'Farenheit'  #Respuesta de solucion
+print(c)
+
---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+'''
+Ejercicio 2: Calculadora de Propina 
+Crea un programa que calcule el monto total a pagar en un restaurante, incluyendo una propina del 15% sobre el total de la cuenta.
+'''
+
+Menu = {
+  'entrante': 10,
+  'principal': 15,
+  'segundo': 20,
+  'postre': 8,
+  'cafe': 2,
+  'copa': 15}
+print(Menu)
