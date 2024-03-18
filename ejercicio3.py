5fe5f6ec9fb105f403a143203d50daf9e7efdfb5
davidsequeiro <david.sequeiro@gmail.com>
2024-03-18 21:35:23 +0100

    feat: Ejercicio5

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,10 +3,10 @@ Ejercicio 3: Verificación de Edad
 Escribe un programa que solicite la edad de un usuario y determine si es mayor de edad (mayor o igual a 18 años) o no.
 '''
 
-print('solucion')
-edad = int(input('¿Cual es tu edad?'))
+edad = int(input('¿Cual es tu edad?: '))
 if edad >=18:
   print('eres mayor de edad')
 else:
   print('no eres mayor de edad')
-  
+
+print('solucion')  
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,6 +1,12 @@
-palabra = input('escribe una palabra:')
+'''Ejercicio 4: Contador de Vocales 
+Crea un programa que cuente el número de vocales en una palabra ingresada por el usuario.
+'''
+contar = 0
+palabra = input('escribe una palabra: ')
 vocales = 'a,e,i,o,u,A,E,I,O,U'
 for letra in palabra:
   if letra in vocales:
+    contar += 1
     print(letra)
+print('El total de letras es: ' ,contar)
     
