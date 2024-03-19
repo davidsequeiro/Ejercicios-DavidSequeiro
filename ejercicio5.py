660eb4dfc6f9fc2ce9b876cb665dbc28a45cab91
davidsequeiro <david.sequeiro@gmail.com>
2024-03-19 19:42:01 +0100

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,6 +3,6 @@ Escribe un programa que calcule la suma de todos los números pares del 1 al 100
 '''
 suma_pares = 0
 for numero in range(1,101):
-  if numero %2 == 0:
+  if numero % 2 == 0:
     suma_pares += numero
 print(suma_pares)
