97274588ec9ec040796098b09d41e3502a755237
davidsequeiro <david.sequeiro@gmail.com>
2024-03-21 20:02:50 +0100

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -4,14 +4,15 @@ Escribe un programa que determine el día de la semana correspondiente a un
 número ingresado por el usuario (1 para lunes, 2 para martes, etc.).
 '''
 
-dia= int(input('ingreasa un numero del 1 al 7:'))-1
+dia= int(input('ingreasa un numero del 1 al 7:'))
 
 semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Viernes', 'Sabado', 'Domingo']
 
 if dia > 7:
   print('Numero no valido')
-  
-print(semana[dia])
+ 
+if dia < 7:  
+  print(semana[dia-1])
 
-print(('El '), dia+1, ('dia de la semana es: '), semana[dia])
+  print(('El '), dia, ('dia de la semana es: '), semana[dia-1])
 
