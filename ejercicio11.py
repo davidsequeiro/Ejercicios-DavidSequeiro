17b8bb7d82c5e1857139ac545d59f16cd4248a41
davidsequeiro <163044639+davidsequeiro@users.noreply.github.com>
2024-03-22 00:52:54 +0100

    Update ejercicio11.py

--- ejercicio11.py
+++ ejercicio11.py
@@ -3,15 +3,18 @@ Ejercicio 11: Calculadora de Edad
 Escribe un programa que solicite al usuario su año de nacimiento y calcule su edad
 actual.
 '''
-
+# Solictamos año actual y año nacimiento
 año_actual = int(input('Introduce año actual: '))
 año_nacimiento = int(input('Introduce año de nacimiento: '))
 
+# Aplicamos calculo
 edad= año_actual - año_nacimiento
 
+# Resultado
 print(edad)
 
 '''
+# Obtener fecha hoy y mostrar formato año (consulta ChatGPT
 from datetime import datetime
 año_ahora = datetime.now().year
 '''
