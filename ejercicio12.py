570e184242fe8dd176d3868cb794c4f3ea88b6e0
davidsequeiro <david.sequeiro@gmail.com>
2024-03-21 21:27:42 +0100

    Create ejercicio12.py

---
+++ ejercicio12.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 12: Calculadora de Área de un Rectángulo
+Crea un programa que calcule el área de un rectángulo. El usuario debe ingresar la
+longitud y el ancho del rectángulo.
+'''
+
+longitud = int(input('Introduzca longitud en centimetros: '))
+anchura = int(input('Introduzca anchura en centimetros: '))
+
+area = longitud * anchura
+
+print('El area es: ', area, 'centimetros cuadrados')
