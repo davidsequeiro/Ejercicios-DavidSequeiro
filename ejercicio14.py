935b2c4d215ec0853a2b1e4c4a169759895d9117
davidsequeiro <david.sequeiro@gmail.com>
2024-03-22 22:33:47 +0100

    Create ejercicio14.py

---
+++ ejercicio14.py
@@ -0,0 +1,17 @@
+'''Ejercicio 14: Calculadora de Descuento
+Crea un programa que calcule el precio final de un artículo después de aplicar un
+descuento del 20%.
+'''
+
+# Definimos el descuento
+dto= 0.20
+descuento = 1 - dto
+
+# Solicitamos el importe del articulo
+precio = int(input('Introduzac el importe de su articulo: '))
+
+# Calculamos el imprte final despues de aplicar el descuento
+Total = precio * descuento
+
+# Respuesta
+print(f'el importe de su articulo despues del descuento es: {Total:.2f}')
