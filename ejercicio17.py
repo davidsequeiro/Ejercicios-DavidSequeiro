1adfbf71e47a4ac45baaf62afd6c494f818e9fb1
davidsequeiro <david.sequeiro@gmail.com>
2024-03-22 22:59:23 +0100

    Create ejercicio17.py

---
+++ ejercicio17.py
@@ -0,0 +1,12 @@
+'''Ejercicio 17: Conversor de Millas a Kilómetros
+Escribe un programa que convierta una distancia en millas a kilómetros. Sabiendo
+que 1 milla equivale a 1.60934 kilómetros.
+'''
+
+milla = int(input('Introduce el numero de kilometros a convertir en millas: '))
+
+km = 1.60934
+
+resultado = km * milla
+
+print(milla,'millas son',resultado,'kilometros')
