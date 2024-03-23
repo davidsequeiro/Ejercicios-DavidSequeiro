0ce81fc5535e09a4af72061d8979365df0f5dbc0
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:42:26 +0100

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,6 +1,8 @@
 '''Ejercicio 5: Suma de Números Pares 
 Escribe un programa que calcule la suma de todos los números pares del 1 al 100.
 '''
+
+# definimos contador, establecemos condicion (divisible entre dos) para el rango dado y devolvemos resultado
 suma_pares = 0
 for numero in range(1,101):
   if numero % 2 == 0:
