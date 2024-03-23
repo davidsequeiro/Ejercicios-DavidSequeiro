8f5bb61485e010e20177f18379034b267dfb406c
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 12:59:52 +0100

    Update ejercicio18.py

--- ejercicio18.py
+++ ejercicio18.py
@@ -2,13 +2,15 @@
 Crea un programa que cuente la cantidad de palabras en una oración ingresada por
 el usuario.
 '''
-
+# Solicito frase al usuario
 frase = input('Introduce una frase:')
+# Transformo en lista con los valores introducidos 
 palabra = frase.split()
-print(len(palabra))
+# Solucion
+print('el numero de palabras es:',len(palabra))
 
-
+# solucion con bucle
 total = 0
 for letra in palabra:
   total += 1
-print(total)
+print('el numero de palabras es:',total)
