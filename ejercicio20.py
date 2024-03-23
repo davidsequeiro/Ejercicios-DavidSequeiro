0d6a3f6971a7069979cf2ae86df45751a7bcb740
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 11:55:37 +0100

    Update ejercicio20.py

--- ejercicio20.py
+++ ejercicio20.py
@@ -4,3 +4,15 @@ Crea un programa que sume todos los números en una lista ingresada por el
 usuario.
 '''
 
+# Solicitamos al usuario que ingrese los numeros a su elecion
+numero_selec = input('Ingresa los valores separados por coma: ')
+
+# Definimos una lista con los numeros elegidos por el usuario
+lista = [int(numero) for numero in numero_selec.split(',')]  
+
+# Sumamos los valores
+suma = sum(lista)
+
+# Resultado
+print ('la suma de los valores introducidos es:',suma)
+   
