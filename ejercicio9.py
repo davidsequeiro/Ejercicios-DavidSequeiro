3427b7d75461a9ea285384702b41009f7785f1ed
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:48:38 +0100

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -3,9 +3,10 @@ Ejercicio 9: Conversor de Divisas
 Crea un programa que convierta una cantidad de dólares a euros. Suponiendo que
 1 dólar es igual a 0.85 euros.
 '''
-
+# Solicitamos al usuario el importe en dolares
 valor = float(input('Introduce el valor en dolares: '))
 
+# Definimos la formula y devolvemos resultado
 def conversor():
   return valor * 0.85
 print(conversor())
