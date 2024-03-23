8cf203df0315c7a1e656b911dde7e77d4db1a3d2
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:44:34 +0100

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -2,8 +2,10 @@
 Crea un programa que verifique si una palabra ingresada por el usuario es un
 palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
 '''
-
+# Solicitamos palabra al usuario
 palabra = str(input('¿Cual es tu palabra?: '))
+
+# Definimos funcion para saber si es igual al derecho que al reves y devolvemos resultado
 def es_palindromo(palabra):
   palabra = palabra.lower()
   return palabra == palabra[::-1]
