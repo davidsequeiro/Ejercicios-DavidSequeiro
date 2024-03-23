e608e3995d1bc24a55a9b77e65cdee38c4ad5df2
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:24:51 +0100

    Create ejercicio19.py

---
+++ ejercicio19.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio 19: Verificación de Año Bisiesto
+Escribe un programa que determine si un año ingresado por el usuario es bisiesto o
+no.
+'''
+
+'''
+Cualquier año divisible por 4 es bisiesto
+'''
+
+# Solicitamos año al usuario para definir variable
+año = int(input('Introduzca año para saber si es bisiesto: '))
+
+# Aplicamos la formula
+if año / 4 == 0:
+  print('El año ',año,' no es bisiesto')
+else:
+  print('El año ',año,' es bisiesto')
