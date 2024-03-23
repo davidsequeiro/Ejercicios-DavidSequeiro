86f0b9c8d1234bddd39309f877f09656089c8860
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:39:33 +0100

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,9 +1,13 @@
 '''Ejercicio 4: Contador de Vocales 
 Crea un programa que cuente el número de vocales en una palabra ingresada por el usuario.
 '''
+
+# establecemos un contador y solicitamos la palabra al usuario y definimos los valores a buscar
 contar = 0
 palabra = input('escribe una palabra: ')
 vocales = 'a,e,i,o,u,A,E,I,O,U'
+
+# Establecemos condicion y devolvemos resultado
 for letra in palabra:
   if letra in vocales:
     contar += 1
