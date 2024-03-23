92e386b40de3264351445d6e712f04fb4012d4a4
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:47:23 +0100

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -2,10 +2,16 @@
 Ejercicio 8: Cálculo del Índice de Masa Corporal (IMC)
 Escribe un programa que calcule el Índice de Masa Corporal (IMC) de una persona.
 '''
+
+# Solicitamos las variables al usuario
 altura = float(input('¿Cual es tu altura en metros?: '))
 peso = float(input('¿Cual es tu peso en kilogramos?: '))
+
+# Definimos la formula de calculo
 def imc(peso,altura):
   imc = peso / (altura **2) 
   return imc
+
+# Devolvemos resultado con 2 decimales
 print(f'Tu indice IMC es: {imc(peso,altura):.2f}')
 
