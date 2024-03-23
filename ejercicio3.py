6181b23baf02cc2618f189156a7aceeab85e2e00
davidsequeiro <david.sequeiro@gmail.com>
2024-03-23 18:33:15 +0100

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,7 +3,10 @@ Ejercicio 3: Verificación de Edad
 Escribe un programa que solicite la edad de un usuario y determine si es mayor de edad (mayor o igual a 18 años) o no.
 '''
 
+# Solicitamos la edad al usuario
 edad = int(input('¿Cual es tu edad?: '))
+
+# Definimos condicion y devolvemos resultado
 if edad >=18:
   print('eres mayor de edad')
 else:
