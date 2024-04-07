5bed8e8d267284b626c54e121c494fe9570335ea
davidsequeiro <david.sequeiro@gmail.com>
2024-04-07 11:18:09 +0200

    Update ejercicio5_2.sql

--- SQL/ejercicio5_2.sql
+++ SQL/ejercicio5_2.sql
@@ -1,3 +1,6 @@
 /*
 Escribe una consulta que te muestre la información detallada de los tickets que han comprado las personas que se llaman Irina.
 */
+SELECT * 
+FROM tickets
+WHERE  passenger_name LIKE 'IRINA%';
