9d640788dbb5afa0e43cf49c681695e6e8f56aa0
davidsequeiro <david.sequeiro@gmail.com>
2024-04-07 11:02:10 +0200

    Update ejercicio2_2.sql

--- SQL/ejercicio2_2.sql
+++ SQL/ejercicio2_2.sql
@@ -1,3 +1,6 @@
 /*
 Escribe una consulta que extraiga todas las columnas de la tabla bookings y refleje todas las reservas que han supuesto una cantidad totSELECTal mayor a 1.000.000 (Unidades monetarias).
 */
+SELECT *
+FROM bookings
+WHERE total_amount > 1000000;
