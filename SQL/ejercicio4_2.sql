3d62317bc41394dd2d26caf7f32bee4294605723
davidsequeiro <david.sequeiro@gmail.com>
2024-04-07 11:07:48 +0200

    Create ejercicio4_2.sql

---
+++ SQL/ejercicio4_2.sql
@@ -0,0 +1,3 @@
+/*
+Con el resultado anterior visualizado previamente, escribe una consulta que extraiga los identificadores de vuelo que han volado con un Boeing 737. (Código Modelo Avión = 733)
+*/
