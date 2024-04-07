47fb73acc5c2178f616adc5ed3440428dfdce17e
davidsequeiro <david.sequeiro@gmail.com>
2024-04-07 11:22:39 +0200

    Update ejercicio4_2.sql

--- SQL/ejercicio4_2.sql
+++ SQL/ejercicio4_2.sql
@@ -2,5 +2,5 @@
 Con el resultado anterior visualizado previamente, escribe una consulta que extraiga los identificadores de vuelo que han volado con un Boeing 737. (Código Modelo Avión = 733)
 */
 SELECT *
-FROM aircrafts_data
+FROM flights
 WHERE aircraft_code = '733';
