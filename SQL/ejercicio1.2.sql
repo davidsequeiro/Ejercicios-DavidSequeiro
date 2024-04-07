4728c593f60f00131c95f34fc9c0c884e9fafbd1
davidsequeiro <david.sequeiro@gmail.com>
2024-04-07 10:53:23 +0200

    Update ejercicio1.2.sql

--- SQL/ejercicio1.2.sql
+++ SQL/ejercicio1.2.sql
@@ -1,3 +1,6 @@
 /*
 Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
 */
+SELECT flight_id,flight_no
+FROM flights
+WHERE status = 'On Time';
